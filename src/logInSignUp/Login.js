0f39a19c47df3ca3717b85c04cb370f115c254ea
Sardar Bilal <rockeykhan142@gmail.com>
2023-02-02 00:18:20 +0500

    Api Integration

--- src/logInSignUp/Login.js
+++ src/logInSignUp/Login.js
@@ -1,40 +1,79 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import "./../Style/signup.css";
+import axios from "axios";
+import { LogInContext } from "../Context/loginContext/loginState";
 
-export default function login(props) {
-  const { setPage } = props;
-  const togglePage = () => {
-    setPage("loggedIn");
+export default function Login() {
+  // React Hooks
+  const nav = useNavigate();
+  // Component Varibales
+  const URL = "http://127.0.0.1:2300/api/v1/user/logIn";
+  let warningText = "";
+
+  // Context States
+  const { setLogIn, formData, setFormData } = useContext(LogInContext);
+
+  // Functions
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // POST API for Login
+  const onHandleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(URL, formData, {
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      if (response.data.status === "success") {
+        setLogIn(true);
+        console.log("Loged In");
+        nav("/");
+        warningText = "";
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Wrong Email/Password");
+    }
   };
 
   return (
     <div className="main row flex-row">
       <div className="container-fluid col-lg-5 main-card">
         <h3>LOG INTO YOUR ACCOUNT</h3>
-        <form>
+        <form onSubmit={onHandleSubmit}>
           <div className="form-group">
-            <label forHtml="name">Email address</label>
+            <label htmlFor="name">Email address</label>
             <input
               type="email"
               className="form-control"
               id="email"
+              name="email"
+              onChange={handleChange}
               placeholder="name@example.com"
               required
             />
           </div>
           <div className="form-group">
-            <label forHtml="name">Password</label>
+            <label htmlFor="name">Password</label>
             <input
               type="password"
               className="form-control"
+              onChange={handleChange}
               id="password"
+              name="password"
               placeholder="********"
             />
+            <p className="m-3 bg-danger">{warningText}</p>
           </div>
-          <button type="submit" onClick={togglePage}>
-            Log In
-            {console.log("loggedIn")}
-          </button>
+          <button type="submit">Log In</button>
         </form>
       </div>
     </div>
--- src/logInSignUp/Signup.js
+++ src/logInSignUp/Signup.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./../Style/signup.css";
 
 export default function Signup() {
+  // React Hooks
+  const nav = useNavigate();
+  // Component Variables
+  const URL = "http://127.0.0.1:2300/api/v1/user";
+  // Component States
+  const [formData, setFormData] = useState({});
+  // Component Fucntions
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+  //Post API  for Sign up
+  const onHandleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log(formData);
+      const response = await axios.post(URL, formData, {
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+
+      if (response.data.status === "success") {
+        console.log("SignedUP");
+        nav("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Please Try Again..Something is Very WRong");
+    }
+  };
+
   return (
     <div className="main row flex-row">
       <div className="container-fluid col-lg-5 main-card">
         <h3>Create Your Account</h3>
-        <form>
+        <form onSubmit={onHandleSubmit}>
           <div class="form-group">
             <label forHtml="name">Your Name</label>
             <input
-              type="name   "
+              type="name"
               class="form-control"
+              onChange={handleChange}
+              name="name"
               id="name"
               placeholder="Name"
               required
@@ -22,6 +59,8 @@ export default function Signup() {
             <input
               type="email"
               class="form-control"
+              onChange={handleChange}
+              name="email"
               id="email"
               placeholder="name@example.com"
               required
@@ -32,19 +71,13 @@ export default function Signup() {
             <input
               type="password"
               class="form-control"
+              onChange={handleChange}
+              name="password"
               id="password"
               placeholder="********"
             />
           </div>
-          <div class="form-group">
-            <label forHtml="name">Confirm Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="cPassword"
-              placeholder="********"
-            />
-          </div>
+
           <button type="submit">Sign up</button>
         </form>
       </div>
