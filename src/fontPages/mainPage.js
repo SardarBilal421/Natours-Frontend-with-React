f842a9482d4f74a576ab34b3ec072c6832d12c30
Sardar Bilal <rockeykhan142@gmail.com>
2023-02-02 00:16:12 +0500

    Render Page bases on user is logged in or not

---
+++ src/fontPages/mainPage.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { LogInContext } from "../Context/loginContext/loginState";
+import FrontPage from "./FrontPage";
+import ProfilePage from "./ProfilePage";
+
+export default function MainPage() {
+  //Context Login State
+
+  const { logIn } = useContext(LogInContext);
+
+  //Chose pages depends on LogIn state
+
+  return <>{logIn ? <ProfilePage /> : <FrontPage />}</>;
+}
